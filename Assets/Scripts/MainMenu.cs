0483dfcc40458f586e55fac75c6fc1606077e160
Giacomo <gmonta2000@gmail.com>
2025-01-09 17:19:48 +0100

    created main menu UI

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] Button resumeGameButton;
+    [SerializeField] TMP_ColorGradient disabledColor;
+    private readonly List<string> fileNames = new()
+    {
+        "/inventoryData.json",
+        "/shopData.json",
+        "/walletData.json"
+    };
+
+    void Start()
+    {
+        if (!IsGameStarted())
+        {
+            Debug.Log("Disabled resume button");
+            resumeGameButton.enabled = false;
+            resumeGameButton.GetComponentInChildren<TextMeshProUGUI>().colorGradientPreset = disabledColor;
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void StartNewGame()
+    {
+        ClearData();
+        PlayGame();
+    }
+
+    private bool IsGameStarted()
+    {
+        return TutorialUI.firstLaunch != 0;
+    }
+
+    private void ClearData()
+    {
+        foreach (string path in fileNames)
+        {
+            string filePath = Application.persistentDataPath + path;
+            if (System.IO.File.Exists(filePath))
+                System.IO.File.Delete(filePath);
+        }
+    }
+}
