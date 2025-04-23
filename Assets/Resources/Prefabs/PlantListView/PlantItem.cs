9846d9973ce660edfb72daf585315a190ee80053
Giacomo <gmonta2000@gmail.com>
2025-04-24 01:10:24 +0200

    upgraded plantview

--- Assets/Resources/Prefabs/PlantListView/PlantItem.cs
+++ Assets/Resources/Prefabs/PlantListView/PlantItem.cs
@@ -7,11 +7,18 @@ public class PlantItem : MonoBehaviour
     [SerializeField] private TextMeshProUGUI plantName;
     [SerializeField] private Image plantIcon;
     [SerializeField] private TextMeshProUGUI plantNumber;
+    [SerializeField] private GameObject plantViewPrefab;
 
     public void SetData(Plant plant, int number)
     {
         plantName.text = plant.name;
         plantIcon.sprite = plant.Icon;
         plantNumber.text = number.ToString();
+
+        GetComponent<Button>().onClick.AddListener(() => {
+            Transform popupContainer = GameObject.Find("SettingUI")?.transform;
+            GameObject plantView = Instantiate(plantViewPrefab, popupContainer);
+            plantView.GetComponent<PlantView>().SetData(plant);
+        });
     }
 }
---
+++ Assets/Resources/Prefabs/PlantListView/PlantView.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlantView : MonoBehaviour
+{
+    [SerializeField] private Button closeButton;
+    [SerializeField] private Button xButton;
+    [SerializeField] private TextMeshProUGUI Name;
+    [SerializeField] private TextMeshProUGUI Desc;
+    [SerializeField] private Image Icon;
+    [SerializeField] private TextMeshProUGUI Health;
+    [SerializeField] private TextMeshProUGUI Price;
+    [SerializeField] private TextMeshProUGUI Upgrades;
+    [SerializeField] private TextMeshProUGUI CoinPerSec;
+
+    void Start()
+    {
+        closeButton.onClick.AddListener(() => {
+            Destroy(gameObject);
+        });
+        xButton.onClick.AddListener(() => {
+            Destroy(gameObject);
+        });
+    }
+
+    public void SetData(Plant plant)
+    {
+        Name.text = plant.Name;
+        Desc.text = plant.Desc.Replace("\\n", "\n");
+        Icon.sprite = plant.Icon;
+        Health.text = plant.Health.ToString();
+        Price.text = plant.Price.ToString();
+        Upgrades.text = (plant.Upgrades.Length - 1).ToString();
+        CoinPerSec.text = plant.CoinPerSecond.ToString();
+    }
+}
